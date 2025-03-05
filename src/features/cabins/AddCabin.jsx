1f88596beeba7d899af43d247ba9756bb627bf0f
pranalYadav <yadav.pranal94@gmail.com>
2025-03-05 12:54:33 +0530

    compound component

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+
 import Button from '../../ui/Button';
 import Modal from '../../ui/Modal';
 import CreateCabinForm from './CreateCabinForm';
@@ -21,12 +21,16 @@ import CreateCabinForm from './CreateCabinForm';
 
 
 export default function AddCabin() {
-    return <Modal>
-        <Modal.Open opens='cabin-form'>
-            <Button>Add new Cabin</Button>
-        </Modal.Open>
-        <Modal.Window name='cabin-form'>
-            <CreateCabinForm />
-        </Modal.Window>
-    </Modal>
+    return (
+        <div>
+            <Modal>
+                <Modal.Open opens='cabin-form'>
+                    <Button>Add new Cabin</Button>
+                </Modal.Open>
+                <Modal.Window name='cabin-form'>
+                    <CreateCabinForm />
+                </Modal.Window>
+            </Modal>
+        </div>
+    )
 }
